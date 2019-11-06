0053a5de541f6c11eaa45a6049ccbb5939302877
IKKI2000 <ikki2002@qq.com>
2019-11-06 10:51:55 +0800

    综合表单Ajax提交新增评级框~

--- admin/views/dashboard/forms/form_ajax.js
+++ admin/views/dashboard/forms/form_ajax.js
@@ -321,6 +321,31 @@ $(function () {
         autoClose: false,
         value: ['2101', '4301', '4302']
     });
+    // 自我评价
+    $('#evaluation').kendoRating({
+        max: 6,
+        label: {
+            template:
+                '# if (value === 1) { #' +
+                '不合格' +
+                '# } else if (value === 2) { #' +
+                '待提升' +
+                '# } else if (value === 3) { #' +
+                '合格' +
+                '# } else if (value === 4) { #' +
+                '良好' +
+                '# } else if (value === 5) { #' +
+                '优秀' +
+                '# } else if (value === 6) { #' +
+                '完美' +
+                '# } #'
+        }
+    }).data('kendoRating').wrapper.kendoTooltip({
+        filter: '.k-rating-item',
+        content: function (e) {
+            return e.target.data('value') + '分';
+        }
+    });
     // 头像
     $('#photo').kendoUpload({
         async: {
@@ -403,7 +428,7 @@ $(function () {
             'pdf'
         ]
     });
-    // 同意条款
+    // 同意
     $('#agree').kendoSwitch({
         messages: {
             checked: '',
