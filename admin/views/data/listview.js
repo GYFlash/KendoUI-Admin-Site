afd424f852fa5f0c36a0696862c6cae8bacf4a68
IKKI2000 <ikki2002@qq.com>
2019-11-14 08:53:26 +0800

    数据列表视图新增评级框模拟数据~

--- admin/views/data/listview.js
+++ admin/views/data/listview.js
@@ -66,6 +66,7 @@ $(function () {
                         color: { type: 'string' },
                         constellation: { type: 'object' },
                         tourism: { type: 'object' },
+                        evaluation: { type: 'number' },
                         summary: { type: 'string' },
                         photo: { type: 'object' },
                         sign: { type: 'string' }
